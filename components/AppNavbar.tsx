67b1297df194b261b4bb7ebe97c52fec78668e2d
derickeroy2123 <derickeroy2123@gmail.com>
2026-04-25 20:40:27 +0800

    refactor: hide AppNavbar on unknown routes

--- components/AppNavbar.tsx
+++ components/AppNavbar.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import { links } from "@/constants";
 import { uppercase } from "@/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const routes = Object.entries(links).map(([key, value]) => ({
   text: uppercase(key),
   href: value,
 }));
 
+const paths = new Set(Object.values(links));
+
 export default function AppNavbar() {
+  const pathname = usePathname();
+
+  if (!paths.has(pathname)) {
+    return;
+  }
+
   return (
     <nav>
       <ul>
