1b21ae9d7e5ca9071411dec586546a113112cbf0
derickeroy2123 <derickeroy2123@gmail.com>
2026-04-25 17:39:19 +0800

    feat: create unstyled quiz page

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,3 +1,85 @@
+"use client";
+
+import { useQuizContext } from "@/contexts/QuizContext";
+
 export default function QuizPage() {
-  return <div>QuizPage</div>;
+  return (
+    <main>
+      <Progress />
+      <Question />
+    </main>
+  );
 }
+
+function Progress() {
+  const {
+    page: { current, max },
+  } = useQuizContext();
+
+  const progress = (current / max) * 100;
+
+  return (
+    <div>
+      <span>
+        Question {current} of {max}
+      </span>
+      <div>
+        <div style={{ width: `${progress}%` }} />
+      </div>
+    </div>
+  );
+}
+
+function Question() {
+  return (
+    <article>
+      <Question.Text />
+      <Question.Choices />
+      <Question.Navigator />
+    </article>
+  );
+}
+
+const Text = () => {
+  const {
+    quiz: {
+      current: { question },
+    },
+  } = useQuizContext();
+
+  return <h1>{question}</h1>;
+};
+
+const Choices = () => {
+  const {
+    quiz: {
+      current: { choices },
+    },
+    answer,
+  } = useQuizContext();
+
+  return (
+    <ul>
+      {choices.map((choice, i) => (
+        <li key={i}>
+          <button onClick={() => answer(i)}>{choice}</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const Navigator = () => {
+  const { previous, next } = useQuizContext();
+
+  return (
+    <div>
+      <button onClick={previous}>Back</button>
+      <button onClick={next}>Next Question</button>
+    </div>
+  );
+};
+
+Question.Text = Text;
+Question.Choices = Choices;
+Question.Navigator = Navigator;
