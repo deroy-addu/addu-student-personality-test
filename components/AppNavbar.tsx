bb9d3f58a424e10a715525431b7797585347ee5e
derickeroy2123 <derickeroy2123@gmail.com>
2026-04-26 13:33:50 +0800

    refactor: add style if navbar link is active

--- components/AppNavbar.tsx
+++ components/AppNavbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { links } from "@/constants";
-import { uppercase } from "@/utils";
+import { cn, uppercase } from "@/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -25,16 +25,24 @@ export default function AppNavbar() {
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-semibold">ADDU Student Personality Test</h1>
         <ul className="flex gap-8">
-          {routes.map((link, i) => (
-            <li key={i}>
-              <Link
-                href={link.href}
-                className="hover:text-text-primary text-text-secondary"
-              >
-                {link.text}
-              </Link>
-            </li>
-          ))}
+          {routes.map(({ text, href }, i) => {
+            const isActive =
+              href === "/" ? pathname === "/" : pathname.startsWith(href);
+
+            return (
+              <li key={i}>
+                <Link
+                  href={href}
+                  className={cn(
+                    "hover:text-text-primary text-text-secondary",
+                    isActive && "text-text-primary underline",
+                  )}
+                >
+                  {text}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
         <button className="primary-action">Login</button>
       </div>
