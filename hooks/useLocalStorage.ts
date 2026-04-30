ad2a0cbf83f53d07f0936327fad7ca3b171e33fc
derickeroy2123 <derickeroy2123@gmail.com>
2026-04-30 15:41:26 +0800

    chore: add a simple useLocalStorage hook

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,28 @@
+import React from "react";
+
+export function useLocalStorage<T>(key: string, initial?: T) {
+  const getInitial = (): T | undefined => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? (JSON.parse(item) as T) : initial;
+    } catch (error) {
+      console.error(error);
+      return initial;
+    }
+  };
+
+  const [value, setStoredValue] = React.useState<T | undefined>(getInitial);
+
+  const set = (v: T | ((prev: T | undefined) => T)) => {
+    try {
+      const newValue = v instanceof Function ? v(value) : v;
+
+      setStoredValue(newValue);
+      window.localStorage.setItem(key, JSON.stringify(newValue));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [value, set];
+}
