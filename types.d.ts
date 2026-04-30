5d05988376dce5f02072cea39467818c71e4ec93
derickeroy2123 <derickeroy2123@gmail.com>
2026-04-30 15:53:12 +0800

    chore: add QuizResults interface

--- types.d.ts
+++ types.d.ts
@@ -7,3 +7,37 @@ type PersonalityScore = Record<Personalities, number>;
  * Shows how much of a characteristic the respondent possess.
  */
 type CharacteristicScore = Record<Characteristics, number>;
+
+/**
+ * The quiz results data that is stored in the storage.
+ */
+interface QuizResults {
+  score: {
+    /**
+     * The personality score result.
+     */
+    personality: PersonalityScore;
+
+    /**
+     * The characteristic score result.
+     */
+    characteristic: CharacteristicScore;
+  };
+
+  quiz: {
+    /**
+     * Questions used on the quiz. In parallel with `choices`.
+     */
+    questions: string[];
+
+    /**
+     * Choices for the questions. In parallel with `questions`.
+     */
+    choices: string[][];
+
+    /**
+     * Chosen indices for choices. In parallel with `choices`.
+     */
+    answers: number[];
+  };
+}
