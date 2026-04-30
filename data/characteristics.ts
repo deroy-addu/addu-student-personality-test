ba5a4ef88bedb4738237e811340c82b57f0845b8
derickeroy2123 <derickeroy2123@gmail.com>
2026-04-30 17:34:41 +0800

    chore: add more characteristics

--- data/characteristics.ts
+++ data/characteristics.ts
@@ -10,4 +10,135 @@ export const characteristics = {
     [Personalities.Balanced]: 0.7,
     [Personalities.Responsible]: 1,
   },
+
+  [Characteristics.Discipline]: {
+    [Personalities.Procrastinator]: 0.1,
+    [Personalities.Crammer]: 0.4,
+    [Personalities.Balanced]: 0.7,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.Consistency]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.4,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.Focus]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.6,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.StressHandling]: {
+    [Personalities.Procrastinator]: 0.3,
+    [Personalities.Crammer]: 0.2,
+    [Personalities.Balanced]: 0.7,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.PlanningAbility]: {
+    [Personalities.Procrastinator]: 0.1,
+    [Personalities.Crammer]: 0.3,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.MotivationStability]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.5,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.Accountability]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.4,
+    [Personalities.Balanced]: 0.7,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.WorkEthic]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.5,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.Adaptability]: {
+    [Personalities.Procrastinator]: 0.6,
+    [Personalities.Crammer]: 0.5,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 0.9,
+  },
+  [Characteristics.Prioritization]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.5,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.Efficiency]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.6,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.SelfControl]: {
+    [Personalities.Procrastinator]: 0.1,
+    [Personalities.Crammer]: 0.4,
+    [Personalities.Balanced]: 0.7,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.LearningSpeed]: {
+    [Personalities.Procrastinator]: 0.3,
+    [Personalities.Crammer]: 0.6,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.Reliability]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.5,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+  [Characteristics.GoalOrientation]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.5,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.Resilience]: {
+    [Personalities.Procrastinator]: 0.3,
+    [Personalities.Crammer]: 0.6,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.AttentionToDetail]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.5,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.Independence]: {
+    [Personalities.Procrastinator]: 0.3,
+    [Personalities.Crammer]: 0.6,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
+
+  [Characteristics.DecisionMaking]: {
+    [Personalities.Procrastinator]: 0.2,
+    [Personalities.Crammer]: 0.5,
+    [Personalities.Balanced]: 0.8,
+    [Personalities.Responsible]: 1,
+  },
 };
